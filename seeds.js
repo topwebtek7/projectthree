df764d31ff00508c9ad43008a9070b76c07b5648
hernquist <hernquistdavid@gmail.com>
2017-04-07 17:43:48 -0400

    updated schema pointsEarned and pointsMax and new seed data

--- seeds.js
+++ seeds.js
@@ -1,19 +1,34 @@
 var mongoose = require('mongoose');
 var Student = require('./models/student.js');
 
+
 mongoose.Promise = global.Promise;
 
 var db = mongoose.connect('mongodb://localhost/project-three');
 //var db = mongoose.connect('mongodb://localhost:27017/criminals-app');
 
+var seededAssignments = [ {
+	name: 'Test on Plants',
+	assignmentType: 'Test',
+	dateCreated: '',
+	pointsEarned: 0, 
+	pointsMax: 20},
+
+	{
+	name: 'Deciduous Project',
+	assignmentType: 'Project',
+	dateCreated: '', 
+	pointsEarned: 0,
+	pointsMax: 50}	
+];
+
 var students = [
-	{ firstName: 'David', lastName: 'Davis', assignments: []},
-	{ firstName: 'Aylin', lastName: 'Jobs', assignments: []},
-	{ firstName: 'Gibby', lastName: 'Smith', assignments: []},
-	{ firstName: 'Jenny', lastName: 'Davis', assignments: []},
-	{ firstName: 'Jill', lastName: 'Davis', assignments: []},
-	];
-	
+	{ firstName: 'David', lastName: 'Davis', assignments: [seededAssignments]},
+	{ firstName: 'Aylin', lastName: 'Jobs', assignments: [seededAssignments]},
+	{ firstName: 'Gibby', lastName: 'Smith', assignments: [seededAssignments]},
+	{ firstName: 'Jenny', lastName: 'Davis', assignments: [seededAssignments]},
+	{ firstName: 'Jill', lastName: 'Davis', assignments: [seededAssignments]},
+	];	
 
 Student.remove({})
   .then(function(){
