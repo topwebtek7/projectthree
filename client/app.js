39bb4562af03d19e48e8944fe90437d95e794745
datdudejibril <datdudejibril@gmail.com>
2017-04-07 18:19:53 -0400

    app.js

--- client/app.js
+++ client/app.js
@@ -1,3 +1,31 @@
 const angular = require('angular');
+require('angular-ui-router');
 
-angular.module('projectThree', []);
+angular
+  .module('gradeBook', []);
+  .config(uiRouterSetup);
+
+uiRouterSetup.$inject = ['$stateProvider', '$urlRouterProvider'];
+function uiRouterSetup($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      template: '<home></home>'
+    })
+    .state('login', {
+      url: '/login',
+      template: '<login></login>'
+    })
+    .state('signin', {
+      url: '/signin',
+      template: '<signin></signin>'
+    })
+    .state('show', {
+      url: '/show',
+      template: '<show></show>'
+    });
+
+    $urlRouterProvider.otherwise('/');
+}
+
