b2c85c0c85c9ce89b69acce7a29546bdef6e26f6
aylinmarie <aylinmariedesigns@gmail.com>
2017-04-09 12:09:10 -0400

    create Auth Service file

---
+++ client/services/auth.service.js
@@ -0,0 +1,20 @@
+// angular
+// 	.module('gradeBook')
+// 	.service('AuthService', AuthService);
+//
+// AuthService.$inject = ['$http', '$state', 'Notification'];
+// function AuthService($http, $state, Notification) {
+//     const self = this;
+//
+//     self.logUserIn = logUserIn;
+//
+//     // function logUserIn(credentials) {
+//     //     return $http
+//     //         .post('/api/sessions', credentials)
+//     //         .then(function onSuccessDoThis(res) {
+//     //             $state.go('foldersIndex');
+//     //         }, function onErrorDoThis(res) {
+//     //             Notification.errorMessage(res.message);
+//     //         });
+//     // }
+// }
